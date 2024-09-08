303c96787d9c9d03cb2316501fb03e6cc347eb37
noobie <daniilnestajko@gmail.com>
2024-09-08 16:39:26 +0200

    Update server.js

--- register/server.js
+++ register/server.js
@@ -23,4 +23,46 @@ app.post('/register', (req, res) => {
 			return res.status(500).json({ error: 'An error occurred. Please try again.' });
 		}
 
-		const
+		const user = new User({
+			username,
+			email,
+			password: hash,
+			approved: false
+		});
+
+		user.save((err) => {
+			if (err) {
+				return res.status(500).json({ error: 'An error occurred. Please try again.' });
+			}
+
+			res.json({ message: 'User created successfully. Please wait for approval.' });
+		});
+	});
+});
+
+app.get('/approve/:id', (req, res) => {
+	const id = req.params.id;
+
+	User.findById(id, (err, user) => {
+		if (err) {
+			return res.status(404).json({ error: 'User not found' });
+		}
+
+		user.approved = true;
+		user.save((err) => {
+			if (err) {
+				return res.status(500).json({ error: 'An error occurred. Please try again.' });
+			}
+
+			res.json({ message: 'User approved successfully' });
+		});
+	});
+});
+
+app.get('/home', (req, res) => {
+	res.sendFile(__dirname + '/home.html');
+});
+
+app.listen(3000, () => {
+	console.log('Server started on port 3000');
+});
